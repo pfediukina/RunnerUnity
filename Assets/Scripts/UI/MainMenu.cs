1993793f303a482cbde4ea88055462084535e91c
Polina Fediukina <61268493+pfediukina@users.noreply.github.com>
2023-02-21 16:34:26 +0200

    tap dotween  + sort
    sorted files + added dotween anims to text in main menu

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -3,9 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using Firebase.Auth;
+using DG.Tweening;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private TMPro.TextMeshProUGUI _tapText;
+
+    private void Start()
+    {
+        _tapText.transform.DOScale(Vector3.one * 1.2f, 0.5f).SetLoops(-1, LoopType.Yoyo);
+    }
+
     public void ChangeScene(int sceneID)
     {   
         FirebaseAuth.DefaultInstance.SignOut();
